c40450299324a0d21a2f29516a5f4f0a748c5700
Cxyu629 <chowxingyu123@gmail.com>
2026-03-10 09:58:32 +0800

    refactor: rename files with "person" to "contact

--- src/main/java/seedu/address/ui/ContactCard.java
+++ src/main/java/seedu/address/ui/ContactCard.java
@@ -12,9 +12,9 @@ import seedu.address.model.contact.Contact;
 /**
  * An UI component that displays information of a {@code Person}.
  */
-public class PersonCard extends UiPart<Region> {
+public class ContactCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "ContactListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -44,7 +44,7 @@ public class PersonCard extends UiPart<Region> {
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
-    public PersonCard(Contact person, int displayedIndex) {
+    public ContactCard(Contact person, int displayedIndex) {
         super(FXML);
         this.person = person;
         id.setText(displayedIndex + ". ");
