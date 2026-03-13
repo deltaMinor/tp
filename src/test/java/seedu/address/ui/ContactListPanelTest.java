8457e2e9b3c586b3b17a644f80971cf48b9c3d7a
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-03-13 13:38:01 +0800

    test: Add ContactListPanel tests

--- src/test/java/seedu/address/ui/ContactListPanelTest.java
+++ src/test/java/seedu/address/ui/ContactListPanelTest.java
@@ -1,6 +1,9 @@
 package seedu.address.ui;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static seedu.address.testutil.TypicalContacts.ALICE;
+import static seedu.address.testutil.TypicalContacts.BENSON;
+import static seedu.address.testutil.TypicalContacts.CARL;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,20 +15,35 @@ import seedu.address.model.contact.Contact;
  * Contains tests for {@code ContactListPanel}.
  */
 public class ContactListPanelTest extends GuiUnitTest {
-    private ObservableList<Contact> contacts = FXCollections.observableArrayList();
+    private ObservableList<Contact> contacts = FXCollections.observableArrayList(ALICE, BENSON, CARL);
     private ContactListPanel contactListPanel = new ContactListPanel(contacts);
+    private ContactListPanel emptyContactListPanel = new ContactListPanel(FXCollections.observableArrayList());
 
     @Test
-    public void emptyListScrollToTopTest() throws Exception {
+    public void scrollToTopTest() throws Exception {
         runAndWait(() -> {
             assertDoesNotThrow(() -> contactListPanel.scrollToTop());
         });
     }
 
     @Test
-    public void emptyListScrollToBottomTest() throws Exception {
+    public void scrollToBottomTest() throws Exception {
         runAndWait(() -> {
             assertDoesNotThrow(() -> contactListPanel.scrollToBottom());
+        });
+    }
+
+    @Test
+    public void emptyListScrollToTopTest() throws Exception {
+        runAndWait(() -> {
+            assertDoesNotThrow(() -> emptyContactListPanel.scrollToTop());
+        });
+    }
+
+    @Test
+    public void emptyListScrollToBottomTest() throws Exception {
+        runAndWait(() -> {
+            assertDoesNotThrow(() -> emptyContactListPanel.scrollToBottom());
         });
     }
 }
